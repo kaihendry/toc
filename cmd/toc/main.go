474ade631601f4f41607110e7e3a615be111bf46
Kai Hendry <hendry@iki.fi>
2021-01-10 12:20:50 +0800

    Allw stdin for input

--- cmd/toc/main.go
+++ cmd/toc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -8,11 +9,29 @@ import (
 	"github.com/kaihendry/toc"
 )
 
+const Version = "toc 0.0.3"
+
+func usage() {
+	fmt.Fprintf(os.Stderr, Version+`
+
+HTML input with the attribute:
+
+	data-fill-with="table-of-contents"
+
+Will be filled with an ordered list of the headers.
+`)
+	flag.PrintDefaults()
+}
+
 func main() {
+	flag.Usage = usage
+	flag.Parse()
+
 	if len(os.Args) < 2 {
-		fmt.Println("HTML file required")
-		os.Exit(1)
+		toc.Create(os.Stdout, os.Stdin)
+		return
 	}
+
 	html, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatalln(err)
@@ -20,5 +39,4 @@ func main() {
 	defer html.Close()
 
 	toc.Create(os.Stdout, html)
-
 }
